85467468736d9ac7e6c168a92eed54dd54c2ee6a
IrynaYakymenko <irynayakymenko93@gmail.com>
2025-04-26 19:29:26 +0300

    deletePersis

--- src/App.jsx
+++ src/App.jsx
@@ -2,58 +2,69 @@ import "./App.css";
 import ContactForm from "./components/ContactForm/ContactForm";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
-import contacts from "./contacts.json";
-import { useEffect, useState } from "react";
+// import contacts from "./contacts.json";
+// import { useEffect, useState } from "react";
+
+// function App() {
+//   const [cards, setCards] = useState(() => {
+//     const savedCards = JSON.parse(localStorage.getItem("users-card"));
+
+//     if (savedCards && savedCards.length > 0) {
+//       return savedCards;
+//     }
+
+//     const initialCards = contacts.map((item) => ({
+//       ...item,
+//       id: crypto.randomUUID(),
+//     }));
+
+//     localStorage.setItem("users-card", JSON.stringify(initialCards));
+//     return initialCards;
+//   });
+
+//   const [searchValue, setSearchValue] = useState("");
+
+//   const handleAddCards = (card) => {
+//     const newCard = { ...card, id: crypto.randomUUID() };
+//     setCards([...cards, newCard]);
+//   };
+
+//   const handleDeleteCards = (id) => {
+//     setCards(cards.filter((item) => item.id !== id));
+//   };
+
+//   const handleSearch = (e) => {
+//     setSearchValue(e.target.value);
+//   };
+
+//   const filterCards = cards.filter((item) =>
+//     item.name.toLowerCase().includes(searchValue.toLowerCase())
+//   );
+
+//   useEffect(() => {
+//     localStorage.setItem("users-card", JSON.stringify(cards));
+//   }, [cards]);
+
+//   return (
+//     <>
+//       <h2>Phonebook</h2>
+//       <ContactForm handleAddCards={handleAddCards} />
+//       <SearchBox handleSearch={handleSearch} />
+//       <ContactList
+//         contacts={filterCards}
+//         handleDeleteCards={handleDeleteCards}
+//       />
+//     </>
+//   );
+// }
 
 function App() {
-  const [cards, setCards] = useState(() => {
-    const savedCards = JSON.parse(localStorage.getItem("users-card"));
-
-    if (savedCards && savedCards.length > 0) {
-      return savedCards;
-    }
-
-    const initialCards = contacts.map((item) => ({
-      ...item,
-      id: crypto.randomUUID(),
-    }));
-
-    localStorage.setItem("users-card", JSON.stringify(initialCards));
-    return initialCards;
-  });
-
-  const [searchValue, setSearchValue] = useState("");
-
-  const handleAddCards = (card) => {
-    const newCard = { ...card, id: crypto.randomUUID() };
-    setCards([...cards, newCard]);
-  };
-
-  const handleDeleteCards = (id) => {
-    setCards(cards.filter((item) => item.id !== id));
-  };
-
-  const handleSearch = (e) => {
-    setSearchValue(e.target.value);
-  };
-
-  const filterCards = cards.filter((item) =>
-    item.name.toLowerCase().includes(searchValue.toLowerCase())
-  );
-
-  useEffect(() => {
-    localStorage.setItem("users-card", JSON.stringify(cards));
-  }, [cards]);
-
   return (
     <>
       <h2>Phonebook</h2>
-      <ContactForm handleAddCards={handleAddCards} />
-      <SearchBox handleSearch={handleSearch} />
-      <ContactList
-        contacts={filterCards}
-        handleDeleteCards={handleDeleteCards}
-      />
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
     </>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,35 +1,35 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import { contactReduce } from "./contacts/contactsSlice";
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
 import { filterReduce } from "./filters/filtersSlice";
+import { contactReduce } from "./contacts/contactsSlice";
 
-const persistConfig = {
-  key: "root",
-  version: 1,
-  storage,
-};
+// const persistConfig = {
+//   key: "root",
+//   version: 1,
+//   storage,
+// };
 
-const persistedReducer = persistReducer(persistConfig, contactReduce);
+// const persistedReducer = persistReducer(persistConfig, contactReduce);
 
 export const store = configureStore({
-  reducer: { contacts: persistedReducer, filter: filterReduce },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: { contacts: contactReduce, filter: filterReduce },
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware({
+  //     serializableCheck: {
+  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+  //     },
+  //   }),
 });
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
