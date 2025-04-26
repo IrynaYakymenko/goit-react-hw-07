2bd54398ee413d9d8d4d7038c8fce23bc5d0077b
IrynaYakymenko <irynayakymenko93@gmail.com>
2025-04-26 17:26:56 +0300

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,26 @@
+import { useSelector } from "react-redux";
+import Contact from "./Contact";
+import { selectContacts, selectFilter } from "../../redux/selectors";
+
+const ContactList = () => {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectFilter);
+  const filteredContacts = contacts?.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+  return (
+    <div>
+      <ul>
+        {filteredContacts?.map((item) => {
+          return (
+            <li key={item.id}>
+              <Contact id={item.id} name={item.name} number={item.number} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default ContactList;
